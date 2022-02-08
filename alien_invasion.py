90c93701d3e360a7af4a54fd7533d477dcd41121
Andre de Castro Rondi <andre_castro_rondi@hotmail.com>
2022-02-08 14:41:59 -0400

    Criando Cenario, Nave e Projeteis

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import pygame
+import game_functions as gf
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    # Inicializa o jogo e cria um objeto para a tela
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Cria uma espaçonave
+    ship = Ship(ai_settings, screen)
+
+    # Cria um grupo no qual serão armazenados os projéteis
+    bullets = Group()
+
+    # Inicia o laço principal do jogo
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+run_game()
